c920b950328f87dfba5ceb3e94696f4c6cffd281
Roman Stepanov <romik112@yandex.ru>
2017-05-16 14:05:13 +0300

    Обновил Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,15 +13,6 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           src: [
-            '*.html'
-          ],
-          dest: '_build'
-        }]
-      },
-      html: {
-        files: [{
-          expand: true,
-          src: [
             'css/**.css',
             'fonts/**/*.{woff,woff2}',
             'img/**.{png,jpg,gif,svg}',
